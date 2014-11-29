8dd2bceacdcae1541cd7b1dbe4ef26c70185b0d7
Jared P. Jones <jjones@uvora.com>
2014-11-28 22:58:18 -0600

    Added JPQ Save Sheet

--- JPQUtility/JPQUtility/ViewController.swift
+++ JPQUtility/JPQUtility/ViewController.swift
@@ -11,13 +11,22 @@ import Cocoa
 class ViewController: NSViewController {
     
     @IBOutlet var toolbarVisualEffectsView: NSVisualEffectView!
-    @IBOutlet weak var fileScrollView: FileScrollView!
+    @IBOutlet weak var addJPQSegmentButton: NSSegmentedControl!
+    @IBOutlet weak var loadJPQSegmentButton: NSSegmentedControl!
+    
+    var savePanel:NSSavePanel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        savePanel = NSSavePanel()
+        savePanel!.nameFieldLabel = "JPQ Name:"
+        savePanel!.nameFieldStringValue = "file.JPQ"
+        savePanel!.allowedFileTypes = ["JPQ"]
+        savePanel!.allowsOtherFileTypes = false
+        savePanel!.extensionHidden = false
+        savePanel!.canCreateDirectories = true
+
         toolbarVisualEffectsView.state = NSVisualEffectState.FollowsWindowActiveState
         toolbarVisualEffectsView.material = NSVisualEffectMaterial.Titlebar
         toolbarVisualEffectsView.blendingMode = NSVisualEffectBlendingMode.BehindWindow
@@ -26,6 +35,41 @@ class ViewController: NSViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addJPQSegmentAction(sender: NSSegmentedControl) {
+        
+        sender.enabled = false
+        
+        // Since running the savePanel will hault the sender action from returning
+        // we must prompt the save panel asyncronously
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            
+            self.savePanel!.beginWithCompletionHandler { (result: Int) -> Void in
+                
+                if let fileLocation = self.savePanel?.URL?.path
+                {
+                    println("\(fileLocation)\n")
+                }
+                else
+                {
+                    let alert = NSAlert()
+                    alert.addButtonWithTitle("Continue")
+                    alert.messageText = "File Saving Failed!"
+                    alert.informativeText = "The file location you're attemping to save to is invalid aka nil."
+                    alert.alertStyle = NSAlertStyle.WarningAlertStyle
+                    alert.beginSheetModalForWindow(self.view.window!, modalDelegate: self, didEndSelector: nil, contextInfo: nil)
+                }
+                self.addJPQSegmentButton.enabled = true
+            }
+            
+            //dispatch_async must return void
+            return
+        })
+            
+        
+        
+        
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
