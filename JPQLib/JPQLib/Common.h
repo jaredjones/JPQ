00cbca5e046d8ba4d188bae88b82138fbdf0988a
Jared P. Jones <jjones@uvora.com>
2015-01-26 01:00:21 -0600

    Add JPQPopover: Implement Stepper

--- JPQLib/JPQLib/Common.h
+++ JPQLib/JPQLib/Common.h
@@ -23,7 +23,7 @@ enum class JPQFileError : uint32
 static const bool JPQ_DEFAULT_OVERWRITEFILE = false;
 static const uint32_t JPQ_DEFAULT_MAXFILES = 1024;
 static const uint16_t JPQ_DEFAULT_VERSION = 1;
-static const uint8_t JPQ_DEFAULT_FILEPOSITIONSIZEINBYTES = 8;
+static const uint8_t JPQ_DEFAULT_FILEPOSITIONSIZEINBYTES = 4;
 
 static const char* JPQ_SIGNATURE = "JPQ\xff\x0a\x20";
 
--- JPQUtility/JPQUtility/AddJPQPopover.swift
+++ JPQUtility/JPQUtility/AddJPQPopover.swift
@@ -13,6 +13,8 @@ class AddJPQPopover: NSViewController, NSTextFieldDelegate
     @IBOutlet weak var maxNumberOfFilesTextField: NSTextField!
     @IBOutlet weak var filePositionByteSizeTextField: NSTextField!
     @IBOutlet weak var combinedStorageTextField: NSTextField!
+    @IBOutlet weak var maxFilesStepper: NSStepper!
+    @IBOutlet weak var filePositionStepper: NSStepper!
     
     weak var container:FileViewController?
     
@@ -29,6 +31,12 @@ class AddJPQPopover: NSViewController, NSTextFieldDelegate
         super.viewDidLoad()
         maxNumberOfFilesTextField.delegate = self
         filePositionByteSizeTextField.delegate = self
+        
+        maxFilesStepper.integerValue = 1024
+        filePositionStepper.integerValue = 4
+        
+        maxNumberOfFilesTextField.stringValue = String("\(maxFilesStepper.integerValue)")
+        filePositionByteSizeTextField.stringValue = String("\(filePositionStepper.integerValue)")
         // Do view setup here.
     }
     
@@ -49,20 +57,48 @@ class AddJPQPopover: NSViewController, NSTextFieldDelegate
         self.container = cont
     }
     
+    @IBAction func stepperPressed(sender: NSStepper)
+    {
+        if sender == self.maxFilesStepper
+        {
+            self.maxNumberOfFilesTextField.stringValue = self.maxFilesStepper.stringValue
+        }
+        if sender == self.filePositionStepper
+        {
+            var x = self.filePositionStepper.integerValue
+            let y = self.filePositionByteSizeTextField.integerValue
+            
+            if x < y
+            {
+                x = y / 2
+            }
+            else if x > y && x < 8
+            {
+                x = y * 2
+            }
+            else
+            {
+                x = y
+            }
+            
+            self.filePositionStepper.integerValue = x
+            self.filePositionByteSizeTextField.stringValue = ("\(x)")
+        }
+    }
     //Allows only numbers in Textfield
     var lastLength:Int = 0
     override func controlTextDidChange(obj: NSNotification)
     {
         let txtField:NSTextField = obj.object as NSTextField
         let len = txtField.stringValue.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
-        if (len == 0)
+        if len == 0
         {
             return
         }
         var str:NSString = txtField.stringValue
         
         //Check for non-ascii character and remove it
-        if ((len - lastLength) > 1)
+        if (len - lastLength) > 1
         {
             txtField.stringValue = str.substringToIndex(len - (len - lastLength))
             return
@@ -70,12 +106,18 @@ class AddJPQPopover: NSViewController, NSTextFieldDelegate
         
         //Grab character and strip it if it's not a number
         let chr = str.characterAtIndex(len - 1)
-        if (chr < 48 || chr > 57)
+        if chr < 48 || chr > 57
         {
             str = str.substringToIndex(len - 1)
             txtField.stringValue = str
         }
         lastLength = txtField.stringValue.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
+        
+        if obj == maxNumberOfFilesTextField
+        {
+            self.maxFilesStepper.integerValue = txtField.integerValue
+            self.stepperPressed(self.maxFilesStepper)
+        }
     }
     
     override func controlTextDidBeginEditing(obj: NSNotification)
